6016abdb67983c468d4627e63b21a910ca0beb86
AbhayaStha <69980750+AbhayaStha@users.noreply.github.com>
2026-01-24 20:03:30 +1000

    Refactor login steps to use instance variables for page objects and update world context to include ProductsPage and CheckoutPage

--- src/steps/login.steps.ts
+++ src/steps/login.steps.ts
@@ -2,28 +2,29 @@ import { Given, When, Then } from "@cucumber/cucumber";
 import { expect } from "@playwright/test";
 import { CustomWorld } from "../support/world";
 import { LoginPage } from "../pages/LoginPage";
-
-let loginPage: LoginPage;
+import { ProductsPage } from "../pages/ProductsPage";
 
 Given("I open the browser", async function (this: CustomWorld) {
-  loginPage = new LoginPage(this.page);
+  this.loginPage = new LoginPage(this.page);
 });
 
 When("I navigate to {string}", async function (this: CustomWorld, url: string) {
-  await loginPage.navigate(url);
+  await this.loginPage.navigate(url);
 });
 
 When("I login with username {string} and password {string}", async function (this: CustomWorld, username: string, password: string) {
-  await loginPage.login(username, password);
+  await this.loginPage.login(username, password);
+
+  this.productsPage = new ProductsPage(this.page);
 });
 
 Then("I should see the message {string}", async function (this: CustomWorld, message: string) {
   // Check if login successful (Products page) or error message
   if (message === "Products") {
-    const header = await loginPage.getProductsHeader();
+    const header = await this.loginPage.getProductsHeader();
     expect(header).toContain(message);
   } else {
-    const error = await loginPage.getErrorMessage();
+    const error = await this.loginPage.getErrorMessage();
     expect(error).toContain(message);
   }
 });
--- src/support/world.ts
+++ src/support/world.ts
@@ -1,5 +1,10 @@
 import { setWorldConstructor, World, setDefaultTimeout } from "@cucumber/cucumber";
 import { Browser, BrowserContext, Page, chromium } from "@playwright/test";
+import { LoginPage } from "../pages/LoginPage";
+import { ProductsPage } from "../pages/ProductsPage";
+import { CheckoutPage } from "../pages/CheckoutPage";
+
+
 
 setDefaultTimeout(60 * 1000); // 60 seconds
 
@@ -7,6 +12,9 @@ export class CustomWorld extends World {
   browser!: Browser;
   context!: BrowserContext;
   page!: Page;
+  loginPage!: LoginPage;
+  productsPage!: ProductsPage;
+  checkoutPage!: CheckoutPage;
 
   async launchBrowser() {
     this.browser = await chromium.launch({ headless: false });
